01f01e91fd24bc05cafb6bc2da1307fd439e4604
Rafael Almeida <raffields@gmail.com>
2026-04-26 18:37:03 -0300

    Remove nome do Pedro do grupo

--- programa-c/main.c
+++ programa-c/main.c
@@ -7,7 +7,6 @@
  * | Ana Santos Nascimento           | 3036101499 |
  * | Breno Fessel Da Paz             | 3026104340 |
  * | Gabrielli Minhano Barbosa       | 3026103592 |
- * | Pedro Junior da Silva Soares    | 3026104213 |
  * | Rafael Baptista de Almeida      | 3026103546 |
  * | Rafaella Pinto Hahon            | 3026102787 |
  * | Victor Nunes Garcia             | 3026101023 |
