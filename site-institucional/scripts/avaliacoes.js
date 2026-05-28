c47fc90958b86ddfc6e5b0928dc2ce0a9a7f631f
Rafael Almeida <raffields@gmail.com>
2026-05-28 05:25:54 -0300

    Adiciona interatividade no envio de avaliações

--- site-institucional/scripts/avaliacoes.js
+++ site-institucional/scripts/avaliacoes.js
@@ -22,3 +22,52 @@ filtros.forEach(radio => {
         }
     });
 });
+
+// Testando se o navegador tem suporta ao elemento HTML template checando
+// a presença do atributo content no elemento template
+if ("content" in document.createElement("template")) {
+    // Instanciamos as avaliações existentes e o template de cartão
+    const formAvaliacao = document.querySelector("#form-avaliacao"),
+        avaliacoes = document.querySelector(".grade-avaliacoes"),
+        template = document.querySelector("#tmplt-avaliacao");
+
+    formAvaliacao.addEventListener("submit", e => {
+        const clone = document.importNode(template.content, true);
+        let estrelas = ["☆", "☆", "☆", "☆", "☆"],
+            cartao = clone.querySelector(".cartao-avaliacao"),
+            nota = formAvaliacao.querySelector("input[name='estrelas']:checked"),
+            nome = formAvaliacao.querySelector("input[name='nome']"),
+            avaliacao = formAvaliacao.querySelector("textarea[name='avaliacao']");
+
+        formAvaliacao.querySelector("button").disabled = true;
+        formAvaliacao.querySelectorAll("input, textarea").forEach(el => {
+            el.disabled = true;
+        });
+
+        for (let i = 0; i <= nota.value; i++) {
+            estrelas[i] = "★";
+        }
+
+        cartao.querySelector(".nota-estrelas").textContent = estrelas.join("");
+        cartao.querySelector(".autor").textContent = nome.value;
+        cartao.querySelector("blockquote p").textContent = `“${avaliacao.value}”`;
+
+        avaliacoes.appendChild(clone);
+
+        if (parseInt(nota.value, 10) > 3) {
+            alert(`Sua avaliação foi publicada! Obrigado pelas ${nota.value} estrelas!`);
+        } else {
+            alert(`Sua avaliação foi publicada! For favor diga-nos como podemos melhorar!`);
+        }
+
+        formAvaliacao.reset();
+        formAvaliacao.querySelector("button").disabled = false;
+        formAvaliacao.querySelectorAll("input, textarea").forEach(el => {
+            el.disabled = false;
+        });
+
+        e.preventDefault();
+        e.stopPropagation();
+        return false;
+    });
+}
